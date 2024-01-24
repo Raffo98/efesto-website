af4d8ca97d1802d419c924829070daca73779da6
Raffo98 <raf.riccardelli@gmail.com>
2024-01-24 16:51:58 +0100

    update team airtable

--- src/components/teamCard.vue
+++ src/components/teamCard.vue
@@ -1,21 +1,23 @@
 <template>
     <div class="teamcard">
         <div class="teamcard__image">
-            <img src="" alt="">
+            <img :src="props.content.pic">
         </div>
-        <div class="teamcard__role">
-            <p v-html="props.content.fields.role"></p>
-        </div>
-        <div class="teamcard__name">
-            <p v-html="props.content.fields.name"></p>
-        </div>
-        <div class="teamcard__mail">
-            <p class="teamcard__mail__title" v-html="props.titles[0]"></p>
-            <p v-html="props.content.fields.mail"></p>
-        </div>
-        <div class="teamcard__phone">
-            <p class="teamcard__mail__title" v-html="props.titles[1]"></p>
-            <p v-html="props.content.fields.phone"></p>
+        <div class="teamcard__info">
+            <div class="teamcard__info__role">
+                <p v-html="props.content.role"></p>
+            </div>
+            <div class="teamcard__info__name">
+                <p v-html="props.content.name"></p>
+            </div>
+            <div class="teamcard__info__mail">
+                <p class="teamcard__info__mail__title" v-html="props.titles[0]"></p>
+                <p v-html="props.content.mail"></p>
+            </div>
+            <div class="teamcard__info__phone">
+                <p class="teamcard__info__phone__title" v-html="props.titles[1]"></p>
+                <p v-html="props.content.phone"></p>
+            </div>
         </div>
     </div>
 </template>
@@ -26,36 +28,81 @@ const props = defineProps({
     titles: Array
 });
 
+
+
 </script>
 
 <style lang="scss" scoped>
 .teamcard {
     border: 1px solid $color-black !important;
+    width: 49%;
+    padding: 1rem !important;
+    display: flex;
+    flex-direction: row;
+    &__image {
+        width: 36%;
+        img  {
+            object-fit: cover;
+            float: left;
+            max-width: 100%;
+            height: auto; 
 
-    &__role {
-        @include card-role;
-        color: $color-black;
-
+        }
     }
-    &__name {
-        @include text;
-        color: $color-black;
+
+    &__info {
+        width: 64%;
+        margin-left: 1rem !important;
+
+        &__role {
+            @include card-role;
+            color: $color-black;
+            padding-bottom: .25rem !important;
 
 
-    }
-    &__mail {
-        @include section-style;
-        color: $color-black;
+        }
+
+        &__name {
+            @include text;
+            color: $color-black;
+            text-transform: uppercase;
+            padding-bottom: .25rem !important;
 
 
-    }
-    &__phone {
-        @include section-style;
-        color: $color-black;
+
+        }
+
+        &__mail {
+            @include section-style;
+            color: $color-black;
+            padding-bottom: .25rem !important;
 
 
+            &__title {
+                @include paragraph-m;
+                color: $color-orange;
+                text-transform: uppercase;
+                padding-bottom: .25rem !important;
+
+            }
+
+        }
+
+        &__phone {
+            @include section-style;
+            color: $color-black;
+
+            &__title {
+                @include paragraph-m;
+                color: $color-orange;
+                text-transform: uppercase;
+                padding-bottom: .25rem !important;
+
+
+            }
+
+        }
     }
 
 }
-
 </style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -8,11 +8,12 @@
     <div class="about__sections" v-for="section, id in props.content.sections" :key="id">
       <div class="about__sections__section">
         <div class="about__sections__section__subtitle">
-          <h2 v-html="section.title"></h2>
+          <h2 v-html="section.title" :style="{ textTransform: section.id === 'cvsender' ? 'uppercase' : 'lowercase' }"></h2>
         </div>
         <div class="about__sections__section__text" v-html="section.text"></div>
         <div class="about__sections__section__team" v-if="section.id == 'team'">
-          <teamCard v-for="(person, idx) in teamDb" :key="idx" :content="person" :titles="props.content.sections[1].cards.titles" />
+          <teamCard v-for="(person, idx) in teamDb" :key="idx" :content="person"
+            :titles="props.content.sections[1].cards.titles" />
         </div>
       </div>
     </div>
@@ -20,9 +21,9 @@
 </template>
 
 <script setup>
-// import airtable from "@/plugins/airtable.js";
+import airtable from "@/plugins/airtable.js";
 import { ref } from "@vue/runtime-core";
-import axios from 'axios';
+// import axios from 'axios';
 
 import teamCard from "@/components/teamCard.vue";
 
@@ -30,31 +31,55 @@ import teamCard from "@/components/teamCard.vue";
 const props = defineProps({
   content: Object,
 });
-const teamDb = ref();
 
-var app_id = "appVMoyb4khjMiISW";
-var api_key = "keyZ04O2juXZ975sl";
+// var app_id = "appVMoyb4khjMiISW";
+// var api_key = "keyZ04O2juXZ975sl";
 
-axios.get(
-  "https://api.airtable.com/v0/" + app_id + "/team?api_key=" + api_key, {
-  headers: {
-    Authorization: "Bearer " + api_key
-  }
-}
-).then(function (response) {
-  console.log(response.data.records);
-  teamDb.value = response.data.records;
+// axios.get(
+//   "https://api.airtable.com/v0/" + app_id + "/team?api_key=" + api_key, {
+//   headers: {
+//     Authorization: "Bearer " + api_key
+//   }
+// }
+// ).then(function (response) {
+//   console.log(response.data.records);
+//   teamDb.value = response.data.records;
 
-}).catch(function (error) {
-  console.log(error)
-})
+// }).catch(function (error) {
+//   console.log(error)
+// })
 
+// airtable.base('team').select({}).eachPage(function page(records, fetchNextPage) {
 
+//     records.forEach(function(record) {
+//         teamDb.value.push(record.fields); 
+//     });
 
+//  fetchNextPage();
+// })
 
-// console.log(airtable);
+// console.log(teamDb.value);
+const teamDb = ref([]);
+
+console.log("wniwd")
+const fetchTeamData = () => {
+  airtable.base('team').select({}).eachPage(function page(records, fetchNextPage) {
+    records.forEach(async function (record) {
+      await teamDb.value.push({ "name": record.fields.name, "role": record.fields.role, "mail": record.fields.mail, "phone": record.fields.phone, "pic": record.fields.pic[0].url, });
+    });
+
+    console.log(teamDb.value)
+    fetchNextPage();
+  }, function done(err) {
+    if (err) { console.error(err); return; }
+
+  });
+};
+fetchTeamData();
+
 
 </script>
+
 
 <style lang="scss" scoped>
 .about {
@@ -84,7 +109,7 @@ axios.get(
       &__subtitle {
         padding-bottom: 2rem !important;
 
-        f h2 {
+        h2 {
           @include h2;
           color: $color-black;
           text-transform: lowercase;
@@ -99,6 +124,14 @@ axios.get(
         padding-bottom: 2rem !important;
         @include paragraph;
         color: $color-black;
+      }
+
+      &__team {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        gap: 1rem;
+
       }
     }
   }
