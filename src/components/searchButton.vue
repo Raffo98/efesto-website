1493d340af9fc5c898aef3f8997cf8c7db851bec
Raffo98 <raf.riccardelli@gmail.com>
2024-02-12 15:44:08 +0100

    Update searchButton.vue

--- src/components/searchButton.vue
+++ src/components/searchButton.vue
@@ -1,33 +1,81 @@
 <template>
     <div class="search">
-        <label class="custom-field">
-            <input type="text" v-model="input" :placeholder="props.content.text" />
+        <label class="input-lb" @mouseover="isHover = true" @mouseleave=" isHover = false">
+            <input class="input-box" type="text" :placeholder="props.content.text" @focusin="isFocused = true" @focusout="isFocused = false"
+                :style="[isHover || isFocused ? { backgroundPosition: 'left bottom' } : { backgroundPosition: 'right bottom' }]" />
         </label>
     </div>
 </template>
 
 <script setup>
-// import { ref } from "@vue/runtime-core";
+import { ref, watch } from "@vue/runtime-core";
 
 const props = defineProps({
     content: Object,
 });
 
-const input = defineModel()
+// const inputBox = document.querySelector('.input-box');
+const isHover = ref(false);
+const isFocused = ref(false);
 
-// const isHover = ref(false);
+
+
+
+watch(isFocused, (e) => {
+    console.log(e);
+});
+
 
 </script>
 
 <style lang="scss" scoped>
 .search {
+    input[type=text] {
+        width: 100%;
+        position: relative;
+        box-sizing: border-box;
+        height: 41px;
+        border: 1.5px solid $color-orange !important;
+        background: linear-gradient(to right, $color-white 50%, $color-orange 50%);
+        background-size: 200% 100%;
+        padding: 8px 16px !important;
+        transition: all 0.8s ease;
+        color: $color-orange;
+        @include paragraph-m;
+        text-transform: lowercase !important;
+
+        &:hover {
+            &::placeholder {
+                color: $color-orange;
+            }
+        }
+
+
+        &:focus {
+            outline: none;
+        }
+
+        &::placeholder {
+            @include paragraph-m;
+            color: $color-white;
+            white-space: nowrap;
+            transition: color 0.5s ease;
+        }
+
+
+    }
+}
+</style>
+
+
+<!-- .search {
     // border: 1.5px solid $color-orange !important;
-    // // background-color: $color-white;
+    // background-color: $color-white;
     // position: relative;
     // transition: width 0.3s ease;
     // background: linear-gradient(to right, $color-white 50%, $color-orange 50%);
     // background-size: 200% 100%;
-    // // background-position:left bottom;
+    // background-position:left bottom;
     // transition: all 0.8s ease;
 
     &__button {
@@ -72,5 +120,4 @@ const input = defineModel()
 
 .onHover {
     filter: invert(50%) sepia(0%) saturate(0%) hue-rotate(133deg) brightness(1200%) contrast(90%) !important;
-}
-</style>
+} -->
