155f62509b82238e3e51fa4b2838762c94a97a05
Raffaele Riccardelli <37075190+Raffo98@users.noreply.github.com>
2024-11-08 11:36:16 +0100

    update contacts

--- src/views/Contacts.vue
+++ src/views/Contacts.vue
@@ -9,7 +9,11 @@
             <h2 v-html="props.content.info.title"></h2>
         </div>
         <div class="contacts__details">
-            <div class="contacts__details__map"></div>
+            <div class="contacts__details__map">
+                <a :href="props.content.image.link">
+                    <img :src="`${$assetsBasePath}contacts/${props.content.image.name}.jpg`" />
+                </a>
+            </div>
             <div class="contacts__details__info">
                 <div class="contacts__details__info__wrapper" v-for="detail, id in props.content.info.details" :key="id">
                     <div class="contacts__details__info__wrapper__title" v-html="detail.title"></div>
@@ -37,6 +41,10 @@ const props = defineProps({
 .contacts {
     width: 100%;
     padding: 3rem 3rem 0 3rem !important;
+
+    .mobile & {
+        padding: 1rem 1rem 0 1rem !important; 
+    }
 
     &__title {
         padding-bottom: 2rem !important;
@@ -70,8 +78,24 @@ const props = defineProps({
 
     &__details {
         padding-bottom: 3rem !important;
+        display: flex;
+        flex-direction: row;
+        .mobile & {
+            flex-direction: column;
+            padding-bottom: 1rem !important;
+        }
 
-        &__map {}
+        &__map {
+            width: 60%;
+            padding-right: 1rem !important;
+            .mobile & {
+            width: 100%;
+            padding: 0 0 1rem 0 !important;
+            }
+            img {
+                width: 100%;
+            }
+        }
 
         &__info {
             &__wrapper {
